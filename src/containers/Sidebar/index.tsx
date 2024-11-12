eca8ae0324b7c4ce65b353379784e57aace6c2f7
Micael Romão <micael.romao32@gmail.com>
2024-11-12 13:25:14 -0300

    construindo o esqueleto do portfolio

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo/Index'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/mikghl.png" />
+    <Titulo>Micael Romão</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo/Index'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus fugit,
+      sint, corporis iusto perspiciatis labore voluptates enim assumenda harum
+      et inventore, quas ullam? Non minima neque illum amet exercitationem
+      placeat.
+    </p>
+  </section>
+)
+
+export default Sobre
